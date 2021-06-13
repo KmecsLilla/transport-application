08877cebde3600ca5b95edab70fef4a53c5d639a
KmecsLilla <kmecs.lilla@gmail.com>
2021-06-13 23:34:05 +0200

    Implement integration test for delay

--- src/test/java/hu/webuni/transport/lilla/TransportApplicationTests.java
+++ src/test/java/hu/webuni/transport/lilla/TransportApplicationTests.java
@@ -1,13 +1,32 @@
 package hu.webuni.transport.lilla;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import hu.webuni.transport.lilla.dto.DelayDto;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
 class TransportApplicationTests {
 
+	@Autowired
+	WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testDelay() {
+		webTestClient
+			.post()
+			.uri("/api/transportplans/1/delay")
+			.bodyValue(new DelayDto(8, 70))
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody(Integer.class)
+			.isEqualTo(35);
 	}
 
 }
